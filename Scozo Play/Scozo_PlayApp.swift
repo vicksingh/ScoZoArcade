e69b7373f466124b46de134dbaea1179255e8c3b
Vick Singh <ivitaminv@gmail.com>
2026-09-05 21:43:32 +0930

    Initial Commit

---
+++ Scozo Play/Scozo_PlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  Scozo_PlayApp.swift
+//  Scozo Play
+//
+//  Created by Vick Singh on 5/9/2026.
+//
+
+import SwiftUI
+
+@main
+struct Scozo_PlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Scozo PlayTests/Scozo_PlayTests.swift
@@ -0,0 +1,19 @@
+//
+//  Scozo_PlayTests.swift
+//  Scozo PlayTests
+//
+//  Created by Vick Singh on 5/9/2026.
+//
+
+import Testing
+@testable import Scozo_Play
+
+struct Scozo_PlayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
