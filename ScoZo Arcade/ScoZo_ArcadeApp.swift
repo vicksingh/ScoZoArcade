e86e32c72507b3c665aa77ff14b3c800b4e84edc
Vick Singh <ivitaminv@gmail.com>
2026-09-05 16:25:21 +0930

    Initial Commit

---
+++ ScoZo Arcade/ScoZo_ArcadeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoZo_ArcadeApp.swift
+//  ScoZo Arcade
+//
+//  Created by Vick Singh on 5/9/2026.
+//
+
+import SwiftUI
+
+@main
+struct ScoZo_ArcadeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ScoZo ArcadeTests/ScoZo_ArcadeTests.swift
@@ -0,0 +1,19 @@
+//
+//  ScoZo_ArcadeTests.swift
+//  ScoZo ArcadeTests
+//
+//  Created by Vick Singh on 5/9/2026.
+//
+
+import Testing
+@testable import ScoZo_Arcade
+
+struct ScoZo_ArcadeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
